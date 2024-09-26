6c1c2a56ecdf7f9078a90e7d57865ef09456281d
jasmine-green-tea <shelldary@yandex.ru>
2024-09-26 21:56:53 +0300

    class task 1-1

---
+++ class-task-1-1/class-task-1-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace class_task_1_1
+{
+    class Program
+    {
+        private static int a;
+        private static int b;
+
+        static void Main(string[] args)
+        {
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.Write($"Переменная a = {a}, переменная b = {b}");
+            Console.ReadKey();
+        }
+    }
+}
