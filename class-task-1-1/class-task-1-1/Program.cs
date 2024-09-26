312d199cdc3cb0c7aadf74f584e3ba29759f3fe7
jasmine-green-tea <shelldary@yandex.ru>
2024-09-26 22:04:06 +0300

    class task 1-1 input update

--- class-task-1-1/class-task-1-1/Program.cs
+++ class-task-1-1/class-task-1-1/Program.cs
@@ -9,14 +9,17 @@ namespace class_task_1_1
 
         static void Main(string[] args)
         {
+            Console.Write($"Введите a:\n");
             a = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Введите b:\n");
             b = Convert.ToInt32(Console.ReadLine());
 
             a = a + b;
             b = a - b;
             a = a - b;
 
-            Console.Write($"Переменная a = {a}, переменная b = {b}");
+            Console.Write($"Переменная a = {a}, переменная b = {b}\n");
+            Console.Write($"Для завершения программы нажмите любую клавишу... ");
             Console.ReadKey();
         }
     }
